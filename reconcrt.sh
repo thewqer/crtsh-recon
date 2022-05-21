fa0997d611fa038a71464e93cc38b3e7f507354f
wqer <64782240+thewqer@users.noreply.github.com>
2022-05-21 10:58:36 +0200

    Scanning with HTTPX

--- reconcrt.sh
+++ reconcrt.sh
@@ -5,11 +5,18 @@ TARGET=$1
 echo -e "=|= The target is: '$TARGET'"
 echo -e "=|= Extracting the urls from https://crt.sh"
 
-curl -fsSL "https://crt.sh/?q=$TARGET&exclude=expired" | pup 'td :contains("'$TARGET'") text{}' | sort -n | uniq -c | sort -rn | column -t | cut -c 5- > $TARGET.txt
+curl -fsSL "https://crt.sh/?q=$TARGET" | pup 'td :contains("'$TARGET'") text{}' | sort -n | uniq -c | sort -rn | column -t | cut -c 5- > $TARGET.txt
 
 
 echo -e "=|= DONE"
 echo -e "=|= Results saved in $TARGET.txt"
+
+echo -e "Scanning with HTTPX"
+
+httpx -l $TARGET.txt -sc -td
+
+
+echo -e "Scanning done"
 
 
 # IF NEEDED UNCOMMENT THIS: uniq $TARGET.txt >> $TARGET.txt
